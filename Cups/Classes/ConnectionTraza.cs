027fe56cdcf01df22861de7d288c8a914df25a2f
Lemus-EXT <uic26262@contiwan.com>
2020-02-07 11:10:21 -0600

    Versión piezas empaque

---
+++ Cups/Classes/ConnectionTraza.cs
@@ -0,0 +1,331 @@
+﻿
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Cups.Classes
+{
+
+    public class ConnectionTraza
+    {
+        #region Print
+        //public static SerialPrintModel GetInformationSerial(string serial)
+        //{
+        //    serial = serial.ToUpper();
+        //    SerialPrintModel label = new SerialPrintModel();
+
+        //    string oracleConn = "Data Source= tqdb002x.tq.mx.conti.de:1521/tqtrazapdb.tq.mx.conti.de; User Id=consulta; Password= solover";
+
+        //    string oracleConnNpg = "Username =logistica; Password= logistica; Host= 10.218.108.252; Port = 5432; Database = iip";
+
+        //    // string NpgQuery = "select MLFB, big_box, small_box, serial,timedate from units where big_box='459535432Q9' and timedate >='2018%';";
+        //    string NpgQueryList = $"select MLFB, big_box, serial from units where  big_box = '{serial}' and timedate >= '2018%'";
+
+        //    #region Connection Traza
+        //    string query = $"SELECT MLFB, Serial from units WHERE SERIAL = '{serial}'"; //9974S432B1136138
+
+        //    // string query = $"SELECT MLFB, Serial, ordernumber, datetimestamp from ETGDL.boxes WHERE SERIAL = '{serial}'"; //9974S432B1136138
+
+        //    // string query2 = $"SELECT aunitsperbox * aboxperpallet FROM ETGDL.products WHERE MLFB = (Select MLFB from ETGDL.boxes WHERE SERIAL = '{serial}')";
+        //    string delimStr = "_ -'";
+        //    char[] delimiter = delimStr.ToCharArray();
+        //    int start = 0;
+
+        //    try
+        //    {
+        //        using (NpgsqlConnection connectioNpg = new NpgsqlConnection(oracleConnNpg))
+        //        {
+        //            NpgsqlCommand command = new NpgsqlCommand(NpgQueryList, connectioNpg);
+        //            connectioNpg.Open();
+        //            NpgsqlDataReader reader = command.ExecuteReader();
+
+        //            Console.WriteLine(delimiter);
+        //            while (reader.Read())
+        //            {
+        //                string str = reader.GetString(0).ToString();
+        //                for (int i = 0; i < delimiter.Length; i++)
+        //                {
+        //                    start = str.IndexOf(delimiter[i]);
+        //                    if (start != -1)
+        //                        break;
+        //                }
+
+        //                //int end = str.Length -1;
+        //                //int start = str.IndexOf('_');
+        //                if (start == -1)
+        //                {
+        //                    label.MLFB = reader.GetString(0).ToString();
+        //                    label.Serial = reader.GetString(1).ToString();
+        //                }
+        //                else
+        //                {
+        //                    string h = str.Substring(0, start);
+        //                    label.MLFB = h;
+        //                    label.Serial = reader.GetString(1).ToString();
+        //                }
+
+        //            }
+        //            reader.Close();
+        //            connectioNpg.Close();
+        //        }
+        //        string query2 = $"SELECT aunitsperbox * aboxperpallet FROM ETGDL.products WHERE MLFB = '{label.MLFB}'";
+        //        using (OracleConnection connection = new OracleConnection(oracleConn))
+        //        {
+
+        //            connection.Open();
+        //            OracleCommand command2 = new OracleCommand(query2, connection);
+
+        //            OracleDataReader reader2 = command2.ExecuteReader();
+
+        //            while (reader2.Read())
+        //            {
+        //                //label.Quantity = reader2.GetOracleValue(0);
+        //                var uno = reader2.GetOracleValue(0).ToString();
+        //                label.Quantity = int.Parse(uno);
+
+        //            }
+        //            Console.WriteLine("Label: " + label);
+        //            // reader.Close();
+        //            reader2.Close();
+        //            connection.Close();
+        //        }
+
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        return label;
+        //    }
+        //    #endregion
+
+
+        //    #region Cambiar por la version en el server de traza
+        //    //using (OracleConnection connection = new OracleConnection(oracleConn))
+        //    //{
+        //    //    OracleCommand command = new OracleCommand(queryString, connection);
+        //    //    connection.Open();
+        //    //    OracleDataReader reader = command.ExecuteReader();
+        //    //    try
+        //    //    {
+        //    //        while (reader.Read())
+        //    //        {
+        //    //            label.MLFB = reader.GetValue(0).ToString();
+        //    //            label.Order = reader.GetValue(1).ToString();
+        //    //            label.Quantity = 80;
+        //    //            label.Serial = reader.GetValue(2).ToString();
+        //    //            label.PackingType = "Tarima";
+        //    //        }
+        //    //    }
+        //    //    finally
+        //    //    {
+        //    //        // always call Close when done reading.
+        //    //        reader.Close();
+        //    //    } 
+
+        //    //}
+        //    #endregion
+
+        //    #region Borrar
+        //    //SerialPrintModel labelTest = new SerialPrintModel();
+        //    //labelTest.Serial = "1234567890";
+        //    //labelTest.MLFB = "A2C1781540095";
+        //    //labelTest.Quantity = 100;
+        //    //labelTest.PackingType = "Tarima";
+
+        //    //return labelTest;
+        //    #endregion
+
+        //    return label;
+
+        //}
+
+        #endregion
+
+        #region Print v.2
+        public static SerialPrintModel GetInformationSerial(string serial)
+        {
+            List<string> serialBox = new List<string>();
+            serial = serial.ToUpper();
+
+            SerialPrintModel label = new SerialPrintModel();
+
+            string oracleConn = "Data Source= tqdb002x.tq.mx.conti.de:1521/tqtrazapdb.tq.mx.conti.de; User Id=consulta; Password= solover";
+
+            string oracleConnNpg = "Username =logistica; Password= logistica; Host= 10.218.108.252; Port = 5432; Database = iip";
+            
+            
+            #region Connection Traza
+            string query = $"SELECT MLFB, Serial from units WHERE SERIAL = '{serial}'"; //9974S432B1136138
+            
+
+            string delimStr = "_ -'";
+            char[] delimiter = delimStr.ToCharArray();
+            int start = 0;
+            int total = 0;
+            bool mlfb = true;
+
+            try
+            {
+                using (NpgsqlConnection connectioNpg = new NpgsqlConnection(oracleConnNpg))
+                {
+                    string NpgQueryList = $"select MLFB, big_box, serial from units where  big_box = '{serial}' and timedate >= '2018%'";
+                    NpgsqlCommand command = new NpgsqlCommand(NpgQueryList, connectioNpg);
+
+                    connectioNpg.Open();
+                    NpgsqlDataReader reader = command.ExecuteReader();
+
+
+                    Console.WriteLine(delimiter);
+                    while (reader.Read())
+                    {
+
+                        serialBox.Add(reader.GetString(2).ToString());
+
+                        if (mlfb)
+                        {
+                            string str = reader.GetString(0).ToString();
+                            for (int i = 0; i < delimiter.Length; i++)
+                            {
+                                start = str.IndexOf(delimiter[i]);
+                                if (start != -1) {
+                                    mlfb = false;
+                                    break;
+                                }
+                            }
+                            if (start == -1)
+                            {
+                                label.MLFB = reader.GetString(0).ToString();
+                                label.Serial = reader.GetString(1).ToString();
+                            }
+                            else
+                            {
+                                string h = str.Substring(0, start);
+                                label.MLFB = h;
+                                label.Serial = reader.GetString(1).ToString();
+                            } 
+                        }
+
+
+                    }
+                    reader.Close();
+                    connectioNpg.Close();
+                }
+
+                foreach (var item in serialBox)
+                {
+                    using (NpgsqlConnection connectioNpg = new NpgsqlConnection(oracleConnNpg))
+                    {
+                        string NpgQueryTotal = $"SELECT count(BIG_BOX) FROM units WHERE BIG_BOX='{item}'";
+                        NpgsqlCommand command2 = new NpgsqlCommand(NpgQueryTotal, connectioNpg);
+                        connectioNpg.Open();
+                        NpgsqlDataReader reader2 = command2.ExecuteReader();
+                        while (reader2.Read())
+                        {
+                            total = total + int.Parse(reader2.GetString(0).ToString());
+                        }
+                        reader2.Close();
+                        connectioNpg.Close();
+                    }
+                    
+                }
+
+                label.Quantity = total;
+
+
+
+
+
+
+
+                string query2 = $"SELECT aunitsperbox * aboxperpallet FROM ETGDL.products WHERE MLFB = '{label.MLFB}'";
+                //using (OracleConnection connection = new OracleConnection(oracleConn))
+                //{
+
+                //    connection.Open();
+                //    OracleCommand command2 = new OracleCommand(query2, connection);
+
+                //    OracleDataReader reader2 = command2.ExecuteReader();
+
+                //    while (reader2.Read())
+                //    {
+                //        //label.Quantity = reader2.GetOracleValue(0);
+                //        var uno = reader2.GetOracleValue(0).ToString();
+                //        label.Quantity = int.Parse(uno);
+
+                //    }
+                //    Console.WriteLine("Label: " + label);
+                //    // reader.Close();
+                //    reader2.Close();
+                //    connection.Close();
+                //}
+
+            }
+            catch (Exception)
+            {
+
+                return label;
+            }
+            #endregion
+
+
+            #region Cambiar por la version en el server de traza
+            //using (OracleConnection connection = new OracleConnection(oracleConn))
+            //{
+            //    OracleCommand command = new OracleCommand(queryString, connection);
+            //    connection.Open();
+            //    OracleDataReader reader = command.ExecuteReader();
+            //    try
+            //    {
+            //        while (reader.Read())
+            //        {
+            //            label.MLFB = reader.GetValue(0).ToString();
+            //            label.Order = reader.GetValue(1).ToString();
+            //            label.Quantity = 80;
+            //            label.Serial = reader.GetValue(2).ToString();
+            //            label.PackingType = "Tarima";
+            //        }
+            //    }
+            //    finally
+            //    {
+            //        // always call Close when done reading.
+            //        reader.Close();
+            //    } 
+
+            //}
+            #endregion
+
+            #region Borrar
+            //SerialPrintModel labelTest = new SerialPrintModel();
+            //labelTest.Serial = "1234567890";
+            //labelTest.MLFB = "A2C1781540095";
+            //labelTest.Quantity = 100;
+            //labelTest.PackingType = "Tarima";
+
+            //return labelTest;
+            #endregion
+
+            return label;
+
+        }
+
+        #endregion
+    }
+
+    public class SerialPrintModel
+    {
+        public string Serial { get; set; }
+
+        public string MLFB { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string PackingType { get; set; }
+
+        public string Order { get; set; }
+
+    }
+
+}
---
+++ Cups/Forms/Etiqueta/FormEtiquetas.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Cups.Forms.Etiqueta
+{
+    partial class FormEtiquetas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormEtiquetas));
+            this.txtSerial = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.lblError = new System.Windows.Forms.Label();
+            this.lblCorrecto = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSerial
+            // 
+            this.txtSerial.Location = new System.Drawing.Point(153, 170);
+            this.txtSerial.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSerial.MaxLength = 30;
+            this.txtSerial.Name = "txtSerial";
+            this.txtSerial.Size = new System.Drawing.Size(203, 20);
+            this.txtSerial.TabIndex = 0;
+            this.txtSerial.Text = "P5666009500215268";
+            this.txtSerial.TextChanged += new System.EventHandler(this.txtSerial_TextChanged);
+            this.txtSerial.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSerial_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(153, 153);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Serial";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(153, 201);
+            this.btnEnviar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(201, 21);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Imprimir";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(153, 233);
+            this.lblError.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(37, 13);
+            this.lblError.TabIndex = 3;
+            this.lblError.Text = "*Error*";
+            this.lblError.Visible = false;
+            // 
+            // lblCorrecto
+            // 
+            this.lblCorrecto.AutoSize = true;
+            this.lblCorrecto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblCorrecto.Location = new System.Drawing.Point(153, 233);
+            this.lblCorrecto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCorrecto.Name = "lblCorrecto";
+            this.lblCorrecto.Size = new System.Drawing.Size(55, 13);
+            this.lblCorrecto.TabIndex = 4;
+            this.lblCorrecto.Text = "*Correcto*";
+            this.lblCorrecto.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(38, 8);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(431, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormEtiquetas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 314);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblCorrecto);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSerial);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FormEtiquetas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Impresión de etiquetas RFID";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSerial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label lblCorrecto;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
